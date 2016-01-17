df76fe14733a058ca8b98b8fe308a2659f53b7c2
Ivan Pushkin <pushkin13@bk.ru>
2016-01-17 18:31:26 +0300

    fix

--- mult_matrix_client.py
+++ mult_matrix_client.py
@@ -2,6 +2,10 @@
 
 from pysimplesoap.client import SoapClient, SoapFault
 
+MATRIX_W=100
+MATRIX_H=100
+
+
 # create a simple consumer
 client = SoapClient(
     location = "http://localhost:8008/",
@@ -10,31 +14,14 @@ client = SoapClient(
     # trace = True,
     ns = False)
 
-# call the remote method
-response = client.multiplyMatrix(
-    first_matrix=[1, 2, 3, 4, 5, 6, 7, 8, 9],
-    first_matrix_width=3,
-    first_matrix_height=3,
-    second_matrix=[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-    second_matrix_width=5,
-    second_matrix_height=3)
-
-l = []
-for i in range(1, len(response.result_matrix)):
-    l.append(int(response.result_matrix[i]))
-print l
-
-print int(response.result_matrix_width)
-print int(response.result_matrix_height)
-
 
 response = client.multiplyMatrix(
-    first_matrix=range(0, 900),
-    first_matrix_width=30,
-    first_matrix_height=30,
-    second_matrix=range(0, 900),
-    second_matrix_width=30,
-    second_matrix_height=30)
+    first_matrix=range(0, MATRIX_W*MATRIX_H),
+    first_matrix_width=MATRIX_W,
+    first_matrix_height=MATRIX_H,
+    second_matrix=range(0,  MATRIX_W*MATRIX_H),
+    second_matrix_width=MATRIX_W,
+    second_matrix_height=MATRIX_H)
 
 l = []
 for i in range(1, len(response.result_matrix)):
